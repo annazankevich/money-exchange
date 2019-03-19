e9229213da2c46daebd480440d8a5d7b10b70802
Anna Zankevich <annzankevich18@gmail.com>
2019-03-19 10:54:32 +0300

    feat:create money exchange

--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    const coins={};
+    let money= {
+        H:50,
+        Q:25,
+        D:10,
+        N:5,
+        P:1,
+    };
+    if(currency<=0){
+        return coins;
+    }
+    if(currency>=10000){
+        coins.error= "You are rich, my friend! We don't have so much coins for exchange";
+        return coins;
+    }
+    for (let key in money) {
+        if (currency >= money[key]) {
+            const sum = Math.floor(currency/money[key]);
+            currency -= sum * money[key];
+            coins[key] = sum
+        }
+    }
+    return coins;
 }
